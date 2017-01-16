d7a9ec6d6d80070ba53b17d60de1c4b33ae0e4ea
HariKrishna Gaddipati <hari2m@gmail.com>
2017-01-15 18:15:13 -0600

    Added signalr to the project adn created a connection and methods

--- SnooperClient/Program.cs
+++ SnooperClient/Program.cs
@@ -1,15 +1,39 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR.Client;
 
 namespace SnooperClient
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            //Set connection
+            var connection = new HubConnection("http://www.squty.com/signalr/hubs");
+            //Make proxy to hub based on hub name on server
+            IHubProxy myHub = connection.CreateHubProxy("chatHub");
+            //Start connection
+            connection.Start().ContinueWith(task =>
+            {
+                if (task.IsFaulted)
+                {
+                    if (task.Exception != null)
+                        Console.WriteLine("There was an error opening the connection:{0}",
+                            task.Exception.GetBaseException());
+                    else Console.WriteLine("Task has returned null");
+                }
+                else
+                {
+                    Console.WriteLine("Connected");
+                }
+            }).Wait();
+            //connection.StateChanged += connection_StateChanged;
+
+            myHub.On("control", param => {
+                Console.WriteLine(param.message);
+            });
+
+            Console.Read();
+            connection.Stop();
         }
     }
 }
